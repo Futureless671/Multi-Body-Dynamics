c6821f3de32fc11828526e288d397c7efd72137d
Ben Rheinfelder <benjamin.rheinfelder@gmail.com>
2024-01-27 20:50:40 -0700

    Implemented Analytical Orbit Equations

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -6,73 +6,105 @@ using UnityEngine;
 
 public class Body : MonoBehaviour
 {
+    //=================================================
+    //=============== Basic Parameters ================
+    //=================================================
     [SerializeField]
     public string Name;
     [SerializeField]
     private Color color;
     private TrailRenderer trail;
     private SpriteRenderer sprite;
+    private SimController controller;
+    public Body PrimaryBody;
+    public Body ParentBodyOverride;
+    private bool primbodycheck;
+    //=================================================
+    //=========== Physical Input Parameters ===========
+    //=================================================
     public float mass;
     [SerializeField]
     private float radius;
-    public bool PrimaryBody;
     [SerializeField]
-    private float perigee;
+    private float r_i;
     [SerializeField]
-    private float vatperigee;
-    [HideInInspector]
-    public Vector2 position;
-    private SimController controller;
-    private Vector2 x;
-    private Vector2 v;
-    private float time;
+    private float f_i;
+    [SerializeField]
+    private Vector3 v_i;
+    //=================================================
+    //============= Calculated Parameters =============
+    //=================================================
+    private float r;
+    public float f;
+    public Vector3 v;
+    private float a;
+    private Vector3 h;
+    private float p;
+    private float mu;
+    private float epsilon;
+    private float e;
 
     void Awake()
     {
+        primbodycheck = false;
         controller = FindObjectOfType<SimController>();
+        PrimaryBody = controller.PrimaryBody;
+        if(PrimaryBody==null)
+        {
+            PrimaryBody = GetComponent<Body>();
+            primbodycheck = true;
+            print(Name + " primbodycheck: " + primbodycheck);
+        }
         sprite = GetComponent<SpriteRenderer>();
         trail = GetComponent<TrailRenderer>();
-        //trail.enabled = false;
+        trail.enabled = false;
         color = new Color(color.r,color.g,color.b,1.0f);
         sprite.color = color;
         Gradient gradient = new Gradient();
         gradient.SetKeys(new GradientColorKey[] {new GradientColorKey(color, 0.0f), new GradientColorKey(Color.white, 1.0f)}, new GradientAlphaKey[] {new GradientAlphaKey(1.0f, 0.8f), new GradientAlphaKey(0.0f, 1.0f)});
         trail.colorGradient = gradient;
-        x = new Vector2(1,0)*perigee*controller.ScaleFactor;
-        transform.position = x;
-        v = new Vector2(0,1)*vatperigee*controller.ScaleFactor;
-        transform.localScale = radius/controller.PrimaryBody.radius*new Vector3(1,1,1);
-
-        
         trail.enabled = true;
+
+        if(primbodycheck==false)
+        {
+            h = Vector3.Cross(controller.polartocartesian(r_i, f_i),v_i);
+            print(Name + " h: " + h);
+            mu = controller.UGC*(mass+PrimaryBody.mass);
+            print(Name + " mu: " + mu);
+            p = h.magnitude*h.magnitude/mu;
+            print(Name + " p: " + p);
+            epsilon = 0.5f*v_i.magnitude*v_i.magnitude - mu/r_i;
+            a = -mu/(2f*epsilon);
+            e = Mathf.Sqrt(1-(p/a));
+            transform.localScale = new Vector3(1,1,1)*radius/PrimaryBody.radius;
+        }
+    }
+
+    void calcposition()
+    {
+        r = p/(1+e*Mathf.Cos(Mathf.Deg2Rad*f))*controller.ScaleFactor;
     }
 
     void FixedUpdate()
     {
-        time = Time.deltaTime*controller.TimeScale;
-        Vector2 Force = new Vector2(0.0f,0.0f);
-        foreach(Body i in controller.realbodies)
+        if(primbodycheck)
         {
-            if(GetComponent<Body>()==i)
+            transform.position = new Vector3(0.0f,0.0f,0.0f);
+        }
+        else
+        {
+            f = controller.time;
+            calcposition();
+            if(ParentBodyOverride==null)
             {
-                continue;
+                transform.position = controller.polartocartesian(r, f);
             }
             else
             {
-                float rmag = Vector2.Distance(transform.position,i.transform.position)/controller.ScaleFactor;
-                //print(Name+"-"+i.Name+" rmag:"+rmag);
-                Vector2 r = (i.transform.position-transform.position)/controller.ScaleFactor;
-                //print(Name+"-"+i.Name+" r:"+r);
-                Vector2 rhat = r/rmag;
-                float Fmag = controller.UGC*mass*i.mass/Mathf.Pow(rmag,2);
-                Vector2 F = rhat*Fmag;
-                Force = Force+F;
+                transform.position = controller.polartocartesian(r, f) + ParentBodyOverride.transform.position;
             }
+            
         }
-        Vector2 a = (Force/mass)*controller.ScaleFactor;
-        transform.position = x;
-        transform.position -= controller.PrimaryBody.transform.position;
-        v += a*time;
-        x += v*time+0.5f * Mathf.Pow(time,2) * a;
     }
+
 }
--- Assets/Scripts/SimController.cs
+++ Assets/Scripts/SimController.cs
@@ -1,35 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class SimController : MonoBehaviour
 {
-    public Body[] bodies;
+    public List<Body> Bodies = new List<Body>{};
+    public List<Body> realbodies = new List<Body>{};
     public float ScaleFactor;
     public float TimeScale;
-    [HideInInspector]
     public Body PrimaryBody;
-    public List<Body> realbodies = new List<Body> {};
     public float UGC = (float)6.674*Mathf.Pow(10,-11);
-    // Start is called before the first frame update
+    public float time;
+    public bool play;
+    public TimeSpan test;
+    public Vector3 polartocartesian(float r, float f)
+    {
+        return new Vector3(r*Mathf.Cos(Mathf.Deg2Rad*f),r*Mathf.Sin(Mathf.Deg2Rad*f),0);
+    }
     void Start()
     {
-        foreach(Body i in bodies)
+        Body FakePrimaryBody = Bodies[0];
+        float mostmassive = 0;
+        play = false;
+        foreach(Body i in Bodies)
         {
-            Body SpawnedBody = Instantiate(i);
-            realbodies.Add(SpawnedBody);
-            if(i.PrimaryBody==true)
+            if(i.mass>mostmassive)
             {
-                if(PrimaryBody==null)
-                {
-                    PrimaryBody = SpawnedBody;
-                }
-                else
-                {
-                    i.PrimaryBody=false;
-                }
+                FakePrimaryBody = i;
+                mostmassive = FakePrimaryBody.mass;
             }
-            
+        }
+        Bodies.Remove(FakePrimaryBody);
+        PrimaryBody = Instantiate(FakePrimaryBody);
+        realbodies.Add(PrimaryBody);
+        foreach(Body i in Bodies)
+        {
+            Body tmp = Instantiate(i);
+            realbodies.Add(tmp);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(play==true)
+        {
+            time += Time.deltaTime*TimeScale;
         }
     }
 }
